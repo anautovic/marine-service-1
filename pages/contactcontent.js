79a86f938ddd9b8f38bf54bc151b2a6131f9ff94
wilfried fonkeng <afk1995wilfried@gmail.com>
2020-09-17 09:40:14 +0100

    contact

--- pages/contactcontent.js
+++ pages/contactcontent.js
@@ -13,7 +13,7 @@ return(
           <h5 className= "text-center">  Tel: +33 749 449 619/ +239 991 17586</h5>
           <h5 className= "text-center"> Location: Rue morta, ao lado do BISTP Sao Tomé et principe</h5>
           <h5 className= "text-center"> Mail: info@macgregor-ms.com</h5>
-          <h5 className= "text-center"> <a  href="https://www.facebook.com/MacGregorMarineServices/"><FacebookIcon size={40} round={true} /> </a> <a href="https://www.linkedin.com/in/macgregor-marine-services/"> <LinkedinIcon size={40} round={true} /></a><a href="https://www.instagram.com/macgregor_marineservices/"> <InstapaperIcon size={40} round={true} /></a></h5>
+          <h5 className= "text-center"> <a  href="https://www.facebook.com/MacGregorMarineServices/"><FacebookIcon size={40} round={true} /> </a> <a href="https://www.linkedin.com/company/macregormarineservices/"> <LinkedinIcon size={40} round={true} /></a><a href="https://www.instagram.com/macgregor_marineservices/"> <InstapaperIcon size={40} round={true} /></a></h5>
           <br></br>
 
 <br></br>
